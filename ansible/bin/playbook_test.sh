cb6051fd708b5145d1d2faf80eba41dcc7cd4a1b
tsujihashi <tsujihashi42195@gmail.com>
2026-02-12 00:46:12 +0900

    test playbooks

--- ansible/bin/playbook_test.sh
+++ ansible/bin/playbook_test.sh
@@ -1,7 +1,10 @@
-PLAYBOOK_DIR="../playbooks"
-PLAYBOOK="test.yml"
-CONFIG_FILE="../ansible.cfg"
-LOG_FILE="../log/ansible-${PLAYBOOK%.yml}-$(date +%Y%m%d-%H%M).log"
+BINDIR=$(dirname ${BASH_SOURCE})
+
+PLAYBOOK_DIR="${BINDIR}/../playbooks"
+PLAYBOOK="${PLAYBOOK_DIR}/test.yml"
+CONFIG_FILE="${BINDIR}/../ansible.cfg"
+LOG_FILE="${BINDIR}/../log/ansible-${PLAYBOOK%.yml}-$(date +%Y%m%d-%H%M%S).log"
+INVENTORY_FILE="${BINDIR}/../inventory/inventory.ini"
 
 export ANSIBLE_CONFIG=$CONFIG_FILE
 export ANSIBLE_LOG_PATH=$LOG_FILE
@@ -9,6 +12,6 @@ export ANSIBLE_LOG_PATH=$LOG_FILE
 echo "PLAYBOOK=${PLAYBOOK}" >> $ANSIBLE_LOG_PATH
 echo "ANSIBLE_CONFIG=${ANSIBLE_CONFIG}" >> $ANSIBLE_LOG_PATH
 
-pushd $PLAYBOOK_DIR
-ansible-playbook $PLAYBOOK
-popd
+#pushd $PLAYBOOK_DIR
+ansible-playbook $PLAYBOOK -i $INVENTORY_FILE
+#popd
